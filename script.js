1391532a25f671c16c78c268b8648821c371e748
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-01-05 10:17:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Namastey 🙏"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "Welcome to React"
+);
+
+const container = React.createElement("div", {}, [heading1, heading2]);
+// multiple elements are passed as array elements
+
+// console.log(heading); // React Element => Object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// * Used ReactDOM because DOM needs to be manipulated i.e. elements to be added there => the root will be created => the place where I want my react to run
+root.render(container);
